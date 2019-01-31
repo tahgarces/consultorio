da70e9b82a2fb5d0d332ba7dc4b168dc6db74f92
Taynara Garces de Lima <taynaragarces@gmail.com>
2019-01-31 17:20:48 -0200

    criacao do projeto

---
+++ src/main/java/com/example/consultorio/ConsultorioApplication.java
@@ -0,0 +1,14 @@
+package com.example.consultorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultorioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultorioApplication.class, args);
+	}
+
+}
+
