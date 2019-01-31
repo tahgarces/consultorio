d47938b513f5132b1560f6f7486c3cf55d28c27e
Taynara Garces de Lima <taynaragarces@gmail.com>
2019-01-31 20:04:35 -0200

    teste de classe de dominio - Paciente

---
+++ src/main/java/com/example/consultorio/domain/Paciente.java
@@ -0,0 +1,111 @@
+package com.example.consultorio.domain;
+
+import java.io.Serializable;
+
+public class Paciente implements Serializable{
+	
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String nomePaciente;
+	private String nomeResponsavel;
+	private String endereco;
+	private String telefone;
+	private String sexo;
+
+	public Paciente() {
+		
+	}
+
+	public Paciente(Integer id, String nomePaciente, String nomeResponsavel, String endereco, String telefone,
+			String sexo) {
+		super();
+		this.id = id;
+		this.nomePaciente = nomePaciente;
+		this.nomeResponsavel = nomeResponsavel;
+		this.endereco = endereco;
+		this.telefone = telefone;
+		this.sexo = sexo;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNomePaciente() {
+		return nomePaciente;
+	}
+
+	public void setNomePaciente(String nomePaciente) {
+		this.nomePaciente = nomePaciente;
+	}
+
+	public String getNomeResponsavel() {
+		return nomeResponsavel;
+	}
+
+	public void setNomeResponsavel(String nomeResponsavel) {
+		this.nomeResponsavel = nomeResponsavel;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Paciente other = (Paciente) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+
+
+
+}
--- src/main/java/com/example/consultorio/resources/PacienteResource.java
+++ src/main/java/com/example/consultorio/resources/PacienteResource.java
@@ -1,8 +1,13 @@
 package com.example.consultorio.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.consultorio.domain.Paciente;
 
 @RestController
 @RequestMapping(value="/pacientes")
@@ -10,7 +15,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class PacienteResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "rest ok";
+	public List<Paciente> listar() {
+		
+		
+		Paciente paciente1 = new Paciente(1, "Taynara Garces", "Gracilene da Silva", "rua das margaridas", "98984018000", "F");
+		Paciente paciente2 = new Paciente(2, "Maria Garces", "Joana da Silva", "rua das margaridas", "98984018000", "F");
+		
+		List<Paciente> lista = new ArrayList();
+		lista.add(paciente1);
+		lista.add(paciente2);
+		
+		return lista;
 	}
+	
+	
 }
