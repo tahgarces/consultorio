85de20455ad42cbd2b4d8ad67f441fbdc293a465
Taynara Garces de Lima <taynaragarces@gmail.com>
2019-02-06 19:54:13 -0200

    criação da base de paciente

--- src/main/java/com/example/consultorio/domain/Paciente.java
+++ src/main/java/com/example/consultorio/domain/Paciente.java
@@ -2,11 +2,18 @@ package com.example.consultorio.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Paciente implements Serializable{
 	
 	
 	private static final long serialVersionUID = 1L;
-	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nomePaciente;
 	private String nomeResponsavel;
@@ -101,11 +108,5 @@ public class Paciente implements Serializable{
 			return false;
 		return true;
 	}
-	
-	
-	
-	
-
-
 
 }
